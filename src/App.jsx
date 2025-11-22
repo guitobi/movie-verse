91c88bc5a1c26ea878c837479f48b042c802b373
guitobi <skullvisit@gmail.com>
2025-11-22 14:24:01 +0200

    card hardcoded

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './index.css'
+import Card from './components/MovieCard'
+
+const movie = {
+  title: "The Matrix",
+  release_date: "1999-03-30",
+  poster_path: "https://m.media-amazon.com/images/I/71PfZFFz9yL._AC_UF894,1000_QL80_.jpg"
+}
 
 function App() { 
   return (
-    <h1 className='text-3xl text-red-500 font-bold underline'>MovieVerse</h1>
+    <>
+      <h1 className='text-3xl text-red-500 font-bold underline'>MovieVerse</h1>
+      <Card movie={movie}></Card>
+    </>
   )
 }
 
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,11 @@
+function Card({movie}) {
+    return (
+        <div className='rounded-lg shadow-lg hover:scale-[1.05] overflow-hidden w-[240px] transition-all'>
+            <img className='poster poster_path' src={movie.poster_path}></img>
+            <h3 className='title'>Name - {movie.title}</h3>
+            <h2 className='year'>Year - {movie.release_date}</h2>
+        </div>  
+    )
+};
+
+export default Card
